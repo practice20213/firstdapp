409a5f74639d08a230d1f0212457bd1d3ee20fd0
matrixonmonday <74882780+matrixonmonday@users.noreply.github.com>
2021-04-26 18:01:24 -0700

    Updates

---
+++ precache-manifest.02a4e95b0dbdba180fa527aa09c30d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e58814b394179dee28",
+    "url": "/firstdapp/static/css/main.568e9791.chunk.css"
+  },
+  {
+    "revision": "71e58814b394179dee28",
+    "url": "/firstdapp/static/js/main.71e58814.chunk.js"
+  },
+  {
+    "revision": "68434793c4d2576b8175",
+    "url": "/firstdapp/static/css/1.19dd8437.chunk.css"
+  },
+  {
+    "revision": "68434793c4d2576b8175",
+    "url": "/firstdapp/static/js/1.68434793.chunk.js"
+  },
+  {
+    "revision": "ce34723220fc697547c4",
+    "url": "/firstdapp/static/js/runtime~main.ce347232.js"
+  },
+  {
+    "revision": "c76f834bc131ad5f97a25fb64f73f397",
+    "url": "/firstdapp/index.html"
+  }
+];
